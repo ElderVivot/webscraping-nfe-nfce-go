177dd4e9022130cefa1963e3bb5eccc577096bf7
ElderVivot <eldervivot@gmail.com>
2020-11-22 12:26:54 -0300

    feat: add to install certificate

---
+++ src/services/certificates/windows/install-certificates.ts
@@ -0,0 +1,25 @@
+import { exec } from 'child_process'
+import path from 'path'
+import util from 'util'
+
+import { listFiles } from '../../../utils/get-list-files-of-folder'
+
+const execAsync = util.promisify(exec)
+
+export async function mainInstallCertificates (directory: string): Promise<void> {
+    const listFilesCertificates = await listFiles(directory)
+    for (const fileCertificate of listFilesCertificates) {
+        const extensionFile = path.extname(fileCertificate)
+        const nameFileOriginal = path.basename(fileCertificate)
+        const nameFileOriginalSplit = nameFileOriginal.split('-')
+        const password = nameFileOriginalSplit[1].replace(extensionFile, '')
+        const { stdout, stderr } = await execAsync(`certutil -f -user -p ${password} -importPFX My "${fileCertificate}"`)
+        if (stdout) {
+            console.log(`- Certificado ${nameFileOriginalSplit[0]} instalado com sucesso`)
+        }
+        if (stderr) {
+            console.log('- Erro ao instalar certificado: ', stderr)
+        }
+    }
+}
+// mainInstallCertificates('C:/_temp/certificados/teste/ok').then(_ => console.log(_))
