d7b8f694e3340306dfe7d92cbfd375dc42962ecc
ElderVivot <eldervivot@gmail.com>
2020-11-22 12:19:29 -0300

    feat: delete tests don't necessary

--- src/services/certificates/windows/set-default-certificate-regedit.ts
+++ src/services/certificates/windows/set-default-certificate-regedit.ts
@@ -22,31 +22,12 @@ exit`
         }
     )
     await execAsync(`powershell -Command "Start-Process powershell -ArgumentList '-noprofile -file ${pathSetDefaultCertificateRegedit}' -Verb RunAs`)
-    // await execAsync('powershell -Command "Start-Process -FilePath powershell -Verb RunAs -ArgumentList \'New-Item -Path HKLM:\\SOFTWARE\\Policies\\Google\\Chrome -Name AutoSelectCertificateForUrls -Force\'"')
-    // const { stdout, stderr } = await execAsync(`powershell -Command "Start-Process -FilePath powershell -Verb RunAs -ArgumentList 'Set-Itemproperty -Path HKLM:\\SOFTWARE\\Policies\\Google\\Chrome\\AutoSelectCertificateForUrls -Name 1 -Value '{"pattern":"${url}","filter":{"SUBJECT":{"CN":"AGF ${certificade.requerenteCN}","OU":"${certificade.requerenteOU}"}}}''"`)
-    // const { stdout, stderr } = await execAsync('powershell -Command "Set-Itemproperty -Path HKLM:\\SOFTWARE\\Policies\\Google\\Chrome\\AutoSelectCertificateForUrls -Name 1 -Value \'{"pattern":"https://nfe.sefaz.go.gov.br","filter":{"SUBJECT":{"CN":"AGF COMERCIO DE BRINQUEDOS LTDA:18122845000115","OU":"Certificado PJ A1"}}}\'"')
-    // const { stdout, stderr } = await execAsync('powershell -Command "Start-Process -FilePath powershell -Verb RunAs -ArgumentList \'Set-Itemproperty -Path HKLM:\\SOFTWARE\\Policies\\Google\\Chrome\\AutoSelectCertificateForUrls -Name 1 -Value {\"teste\":\"123\",\"teste2\":\"123\"}\'"')
-    // const { stdout, stderr } = await execAsync(`REG ADD HKLM\\SOFTWARE\\Policies\\Google\\Chrome\\AutoSelectCertificateForUrls /v 1 /d {""pattern"":""${url}"",""filter"":{""SUBJECT"":{""CN"":""${certificade.requerenteCN}"",""OU"":""${certificade.requerenteOU}""}}} /f`)
-    // const { stdout, stderr } = await execAsync(`REG ADD HKLM\\SOFTWARE\\Policies\\Google\\Chrome\\AutoSelectCertificateForUrls /v 1 /d {"pattern":"${url}","filter":{"SUBJECT":{"CN":"${certificade.requerenteCN}","OU":"${certificade.requerenteOU}"}}} /f`)
-    // console.log(stdout, stderr)
-    // const ps = new Shell({
-    //     executionPolicy: 'Bypass',
-    //     noProfile: true
-    // })
-
-    // await ps.addCommand('Start-Process PowerShell -Verb RunAs')
-    // await ps.addCommand("'New-Item -Path HKLM:\\SOFTWARE\\Policies\\Google\\Chrome -Name AutoSelectCertificateForUrls -Force'")
-    // await ps.addCommand('Start-Process -FilePath powershell -Verb RunAs -ArgumentList')
-    // await ps.addCommand(`Set-Itemproperty -Path HKLM:\\SOFTWARE\\Policies\\Google\\Chrome\\AutoSelectCertificateForUrls -Name 1 -Value '{"pattern":"${url}","filter":{"SUBJECT":{"CN":"${certificade.requerenteCN}","OU":"${certificade.requerenteOU}"}}}'`)
-    // await ps.addCommand('exit')
-    // await ps.invoke()
-    // ps.invoke().then(_ => console.log(_)).catch(_ => console.log(_))
 }
 
-mainSetDefaultCertificateRegedit('https://nfe.sefaz.go.gov.br', {
-    notAfter: new Date(),
-    notBefore: new Date(),
-    numeroSerie: '',
-    requerenteCN: 'A L R ELETRICA EIRELI:18040800000100',
-    requerenteOU: 'Certificado PJ A1'
-}).then(_ => console.log(_))
+// mainSetDefaultCertificateRegedit('https://nfe.sefaz.go.gov.br', {
+//     notAfter: new Date(),
+//     notBefore: new Date(),
+//     numeroSerie: '',
+//     requerenteCN: 'A L R ELETRICA EIRELI:18040800000100',
+//     requerenteOU: 'Certificado PJ A1'
+// }).then(_ => console.log(_))
