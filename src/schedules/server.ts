38929e2f9d8c7eb22416b4e2dea5dd4ddf8673a7
Elder Vivot <eldervivot@gmail.com>
2021-03-28 12:07:11 -0300

    feat: add server port in .env

--- src/schedules/server.ts
+++ src/schedules/server.ts
@@ -1,4 +1,5 @@
 import app from './app'
+import 'dotenv/config'
 
-const port = 3331
+const port = Number(process.env.SERVER_PORT) || 3337
 app.listen(port, () => console.log(`Executing Server Schedule in port ${port} !`))
