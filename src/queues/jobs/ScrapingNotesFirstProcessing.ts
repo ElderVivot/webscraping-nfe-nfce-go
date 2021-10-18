c1d58ac81b97b78543b78ee13cf2f6cc110f19d5
Elder Vivot <eldervivot@gmail.com>
2021-10-18 05:03:26 -0300

    feat: update to ScpapintNotesFirstProcessing install certificate regedit, because the other way was slow

--- src/queues/jobs/ScrapingNotesFirstProcessing.ts
+++ src/queues/jobs/ScrapingNotesFirstProcessing.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+import { prepareCertificateRegedit } from 'src/services/certificates/windows/prepare-certificate-regedit'
 
 import { ISettingsNFeGoias } from '../../scrapings/nfegoias/ISettingsNFeGoias'
 import { MainNFGoiasAddQueueToProcess } from '../../scrapings/nfegoias/MainNFGoiasAddQueueToProcess'
@@ -7,11 +8,13 @@ export const ScrapingNotesFirstProcessing = {
     key: 'ScrapingNotesFirstProcessing',
     async handle ({ data }): Promise<void> {
         const settings: ISettingsNFeGoias = data.settings
+        const certificate = await prepareCertificateRegedit(settings.wayCertificate)
 
         await MainNFGoiasAddQueueToProcess({
             wayCertificate: settings.wayCertificate,
             hourLog: settings.hourLog,
-            dateHourProcessing: settings.dateHourProcessing
+            dateHourProcessing: settings.dateHourProcessing,
+            nameCompanie: certificate.nameCertificate
         })
         await new Promise((resolve) => setTimeout(() => resolve(''), 1000))
         return Promise.resolve()
